971c0e9a1d46fc84a2c3f14e2dfdd1be32d9e893
Ajay Dalal <ajaydalal199@gmail.com>
2025-07-08 10:43:39 +0530

    updated cors

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,7 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:5173",
   process.env.VERCEL_FRONTEND_URL,
+  "*",
 ];
 app.use(express.json());
 app.use(cors({ origin: allowedOrigins, credentials: true }));
